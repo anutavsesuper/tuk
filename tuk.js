bb59740be986cd7388f6c0c7438a057fb47ebdfe
anutavsesuper <55274885+anutavsesuper@users.noreply.github.com>
2020-06-01 22:46:22 +0300

    Add files via upload

--- tuk.js
+++ tuk.js
@@ -1,66 +1,29 @@
+let img;
+
 let bugs = [];
 
 var middle = 0
 var moddle = -500
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 100; i++) {
+createCanvas(windowWidth, windowHeight);
+  img = loadImage(src = 'https://raw.githubusercontent.com/anutavsesuper/tuk/master/img/krug.png');
+    createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 50; i++) {
     bugs.push(new Love());
   }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
 
-
-function draw() {
-
-  background(255, 30, 0);
-
-  noStroke();
-  textFont('Arial Black');
-  textSize(100);
-  textAlign(LEFT);
-
-  if ((mouseX > 0) && (mouseX < width) && (mouseY > 50) && (mouseY < 120) ||
-  (mouseX > 0) && (mouseX < width) && (mouseY > height - 120) && (mouseY < height - 50)){
-    fill(255, 222, 173)
-  }
-  else {
-    fill(1, 66, 254, 200);
-  }
-
-
-  text("BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  ", middle, 120);
-
-  text("BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  BASKETBALL GAME  23/06  ", moddle, height - 50);
-
-  middle = middle - 2
-
-  moddle = moddle - 2
-
-  textAlign(CENTER)
-  stroke(255, 222, 173);
-  strokeWeight(2)
-  noFill();
-  textSize(200);
-  text("23.06 18:00", width/2, height/1.6)
-
-  for (let i = 0; i < bugs.length; i++) {
-    bugs[i].move();
-    bugs[i].display();
-  }
-}
 
 class Love {
   constructor() {
     this.x = random(width);
     this.y = random(height);
-    this.diameter = random(25, 50);
-    this.speed = 1.2;
+    this.diameter = random(30, 50);
+    this.speed = 1;
   }
+
 
   move() {
     this.x += random(-this.speed, this.speed);
@@ -68,6 +31,197 @@ class Love {
   }
 
   display() {
-    ellipse(this.x, this.y, this.diameter, this.diameter );
-  }
+    // ellipse(this.x, this.y, this.diameter, this.diameter);
+    image(img, this.x, this.y, this.diameter, this.diameter)
 }
+}
+
+function windowResized() {
+resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+background(255, 30, 0);
+
+    for (let i = 0; i < bugs.length; i++) {
+    bugs[i].move();
+    bugs[i].display();
+  }
+
+noStroke();
+textFont('Arial Black');
+textSize(150);
+textAlign(LEFT);
+
+if ((mouseX > 0) && (mouseX < width) && (mouseY > 15) && (mouseY < 120)){
+fill(255, 222, 173);
+text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+fill(1, 66, 254, 200);
+text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 15)
+
+
+}
+else if((mouseX > 0) && (mouseX < width) && (mouseY > height - 120) && (mouseY < height - 15)){
+fill(255, 222, 173);
+text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 15);
+fill(1, 66, 254, 200);
+text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+
+}
+else
+{
+fill(1, 66, 254, 200);
+text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 15);
+}
+
+middle = middle - 2
+
+moddle = moddle - 2
+
+textAlign(CENTER)
+stroke(255, 222, 173);
+strokeWeight(2)
+noFill();
+textSize(150);
+text("23.06 18:00", width/2, height/2)
+
+noStroke()
+fill(255, 222, 173)
+textSize(100);
+text("LIGHT NIGHT", width/2, height/1.43)
+
+
+}
+
+
+
+
+
+
+
+
+// var middle = 0
+// var moddle = -500
+//
+// function setup() {
+  // createCanvas(windowWidth, windowHeight);
+  // }
+  //
+  // function windowResized() {
+    // resizeCanvas(windowWidth, windowHeight);
+    // }
+    //
+    // function draw() {
+      //
+      // background(255, 30, 0);
+      //
+      // noStroke();
+      // textFont('Arial Black');
+      // textSize(150);
+      // textAlign(LEFT);
+      //
+      // if ((mouseX > 0) && (mouseX < width) && (mouseY > 15) && (mouseY < 120)){
+        // fill(255, 222, 173);
+        // text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+        // fill(1, 66, 254, 200);
+        // text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 15)
+        //
+        //
+        // }
+        // else if((mouseX > 0) && (mouseX < width) && (mouseY > height - 120) && (mouseY < height - 15)){
+          // fill(255, 222, 173);
+          // text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 15);
+          // fill(1, 66, 254, 200);
+          // text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+          //
+          // }
+          // else
+          // {
+            // fill(1, 66, 254, 200);
+            // text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+            // text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 15);
+            // }
+            //
+            // middle = middle - 2
+            //
+            // moddle = moddle - 2
+            //
+            // textAlign(CENTER)
+            // stroke(255, 222, 173);
+            // strokeWeight(2)
+            // noFill();
+            // textSize(150);
+            // text("23.06 18:00", width/2, height/2)
+            //
+            // noStroke()
+            // fill(255, 222, 173)
+            // textSize(100);
+            // text("LIGHT NIGHT", width/2, height/1.43)
+            //
+            //
+            // }
+
+
+
+// var middle = 0
+// var moddle = -500
+//
+// function setup() {
+// createCanvas(windowWidth, windowHeight);
+// }
+//
+// function windowResized() {
+// resizeCanvas(windowWidth, windowHeight);
+// }
+//
+// function draw() {
+//
+// background(255, 30, 0);
+//
+// noStroke();
+// textFont('Arial Black');
+// textSize(100);
+// textAlign(LEFT);
+//
+// if ((mouseX > 0) && (mouseX < width) && (mouseY > 50) && (mouseY < 120)){
+// fill(255, 222, 173);
+// text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+// fill(1, 66, 254, 200);
+// text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 50)
+//
+//
+// }
+// else if((mouseX > 0) && (mouseX < width) && (mouseY > height - 120) && (mouseY < height - 50)){
+// fill(255, 222, 173);
+// text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 50);
+// fill(1, 66, 254, 200);
+// text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+//
+// }
+// else
+// {
+// fill(1, 66, 254, 200);
+// text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", middle, 120);
+// text("BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 BASKETBALL GAME 23/06 ", moddle, height - 50);
+// }
+//
+// middle = middle - 2
+//
+// moddle = moddle - 2
+//
+// textAlign(CENTER)
+// stroke(255, 222, 173);
+// strokeWeight(2)
+// noFill();
+// textSize(150);
+// text("23.06 18:00", width/2, height/2)
+//
+// noStroke()
+// fill(255, 222, 173)
+// textSize(100);
+// text("LIGHT NIGHT", width/2, height/1.43)
+//
+//
+// }
